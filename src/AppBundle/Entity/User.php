60d40b41cb50863482065ef7e3f927ae053e903e
Daniel <daniel@allgigs.co.uk>
2015-11-12 15:46:44 +0000

    Use and configure FOSUserBundle

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: daniel
+ * Date: 12/11/15
+ * Time: 14:52
+ */
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="`user`")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+} 
